523a2185cd072afb4a4521d60cc43664a5e2e812
Seyyed Shayan Daneshvar <daneshvarshayan1@gmail.com>
2020-02-20 14:45:28 +0330

    Add Assignment with tests

---
+++ src/main/java/ir/ac/kntu/Car.java
@@ -0,0 +1,44 @@
+package ir.ac.kntu;
+
+/**
+ * @author your name
+ */
+public class Car {
+    private int weight;
+    private String color;
+    private String name;
+
+    // TODO: Write the required constructors
+
+
+
+
+    // TODO: After writing the required constructors, create a Main class and
+    //test your code there.You can use the written unit-tests to test this class
+    // in order to use the unit test you can type <mvn clean test> in
+    // the terminal or use your IDE for running the tests for you.
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/ir/ac/kntu/CarTest.java
@@ -0,0 +1,100 @@
+package ir.ac.kntu;
+
+import org.junit.jupiter.api.*;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author S.Shayan Daneshvar
+ */
+@SuppressWarnings("JavaReflectionMemberAccess")
+public class CarTest {
+    private static Class<Car> carClass;
+    private static final List<Constructor<?>> constructors = new ArrayList<>();
+    private boolean noArgConstructor;
+    private boolean oneArgConstructor;
+    private boolean twoArgConstructor;
+
+    @BeforeAll
+
+    public static void prepare() {
+        carClass = Car.class;
+        constructors.addAll(Arrays.asList(carClass.getConstructors()));
+    }
+
+    @Test
+    public void defaultConstructorTest() {
+        Assertions.assertEquals(1, constructors
+                .stream()
+                .filter(x -> x.getParameterCount() == 0)
+                .count(), "No Default Constructor Found!");
+        System.err.println("Default Constructor Found");
+        noArgConstructor = true;
+    }
+
+    @Test
+    public void oneArgConstructorTest() {
+        constructors.stream()
+                .filter(x -> x.getParameterCount() == 1)
+                .map(z -> z.getParameters()[0].getType().equals(String.class))
+                .findFirst()
+                .ifPresentOrElse(Assertions::assertTrue, Assertions::fail);
+        System.err.println("One Args Constructor Found!");
+        oneArgConstructor = true;
+    }
+
+    @Test
+    public void twoArgsConstructor() {
+        constructors.stream()
+                .filter(x -> x.getParameterCount() == 2)
+                .map(z -> z.getParameters()[0].getType().equals(String.class)
+                        && z.getParameters()[1].getType().equals(String.class))
+                .findFirst()
+                .ifPresentOrElse(Assertions::assertTrue, Assertions::fail);
+        System.err.println("Two Args Constructor Found!");
+        twoArgConstructor = true;
+    }
+
+    @Test
+    @Order(Integer.MAX_VALUE)
+    public void allConstructorsFunctionalityTest() {
+        if (!(noArgConstructor && oneArgConstructor && twoArgConstructor)) {
+            Assertions.fail("Required Constructors not available");
+        }
+        final String hunter = "Hunter";
+        final String blue = "Blue";
+        final String white = "White";
+        final String fride = "Fride";
+        try {
+            Car car = Car.class.getConstructor().newInstance();
+            Assertions.assertEquals(fride, car.getName(), "Wrong name" +
+                    " - default Constructor");
+            Assertions.assertEquals(white, car.getColor(), "Wrong " +
+                    "Color - default Constructor");
+            Car car2 = Car.class.getConstructor(String.class)
+                    .newInstance(hunter);
+            Assertions.assertEquals(hunter, car2.getName(), "Wrong " +
+                    "name - 1 arg Constructor");
+            Assertions.assertEquals(white, car2.getColor(), "Wrong " +
+                    "Color - 1 arg Constructor");
+            Car car1 = Car.class.getConstructor(String.class, String.class)
+                    .newInstance(hunter, blue);
+            Assertions.assertEquals(hunter, car1.getName(), "Wrong " +
+                    "name - 2 args Constructor");
+            Assertions.assertEquals(white, car1.getColor(), "Wrong " +
+                    "Color - 2 Args Constructor");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Assertions.fail("Wrong Constructors");
+        }
+    }
+
+    @Test
+    public void testPackage() {
+        Assertions.assertEquals("ir.ac.kntu",
+                carClass.getPackage().getName());
+    }
+}
